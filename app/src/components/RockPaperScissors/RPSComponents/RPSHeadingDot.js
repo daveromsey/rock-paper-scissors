a86d179d26e0e8e8d46e63073647934ea7c97476
David Romsey <david.romsey@webdevstudios.com>
2022-02-19 19:40:16 -0500

    Fix huge line height

--- app/src/components/RockPaperScissors/RPSComponents/RPSHeadingDot.js
+++ app/src/components/RockPaperScissors/RPSComponents/RPSHeadingDot.js
@@ -2,7 +2,7 @@ const RPSHeadingDot = () => {
 	return (
 		<span className="
 			heading-dot
-			font-sans text-5xl sm:text-8xl leading-none
+			font-sans text-5xl sm:text-8xl leading-[0] sm:leading-[0]
 			relative top-[8px] sm:top-[15px]"
 		>
 			&#183;
