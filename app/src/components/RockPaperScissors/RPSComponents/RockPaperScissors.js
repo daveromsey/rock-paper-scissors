ecd54e66c46ccd364b0f69ba141e65f0b43b1b13
David Romsey <david.romsey@webdevstudios.com>
2022-02-20 00:52:18 -0500

    Pull in chart options from a separate file

--- app/src/components/RockPaperScissors/RPSComponents/RockPaperScissors.js
+++ app/src/components/RockPaperScissors/RPSComponents/RockPaperScissors.js
@@ -33,6 +33,7 @@ import {
 
 import { Radar } from 'react-chartjs-2';
 
+import ChartShotPctOptions from '../ChartShotPctOptions';
 import { useRPSContext } from '../RPSContext';
 import {
 	getShotPercentage,
@@ -60,97 +61,6 @@ const RockPaperScissors = () => {
 		Title,
 		SubTitle
 	);
-
-	const optionsChartJS = {
-		//responsive: false,
-		//maintainAspectRatio: true,
-		elements: {
-			line: {
-				borderWidth: 3
-			}
-		},
-		scales: {
-			r: {
-				// https://www.chartjs.org/docs/latest/axes/radial/
-				angleLines: {
-          color: 'rgba( 0,0,0,.35)',
-        },
-				pointLabels: {
-					font:  {
-						size: 16,
-					},
-					color: '#000',
-				},
-				//display: false,
-				grid: {
-					color: 'rgba( 0,0,0,.35)',
-					//circular: true,
-        },
-				ticks: {
-					display: false,
-					beginAtZero: true,
-					max: 100,
-					min: 0,
-					stepSize: 25,
-					backdropColor: 'transparent',
-					showLabelBackdrop: true,
-					backdropPadding: 1,
-					color: '#000',
-					padding: 9,
-					font: {
-						size: 16,
-					}
-				},
-			}
-		},
-		layout: {
-			// padding: {
-			// 	bottom: 220,
-			// },
-			// autoPadding: false,
-		},
-		plugins: {
-			// https://www.chartjs.org/docs/3.5.0/configuration/tooltip.html
-			tooltip: {
-				callbacks: {
-					label: function(context) {
-						let label = context.dataset.label || '';
-						if (label) {
-							label += ': ';
-						}
-						if (context.parsed.y !== null) {
-							label += new Intl.NumberFormat('en-US', { style: 'percent', maximumFractionDigits: 2, }).format(context.parsed.r);
-						}
-						return label;
-					},
-				},
-			},
-			legend: {
-				display: false,
-				position: 'top',
-				labels: {
-					color: '#000',
-					font: {
-						size: 16
-					},
-				},
-			},
-			title: {
-				display: false,
-				text: 'Shot Usage',
-				padding: {
-					top: 10,
-					bottom: 30
-				},
-				position: 'bottom',
-			},
-			subtitle: {
-				display: false,
-				text: 'Radar Chart',
-				position: 'bottom',
-			},
-		},
-	};
 
 	const playerDataChartJS = {
 		labels: ['Rock', 'Paper', 'Scissors'],
@@ -236,7 +146,7 @@ const RockPaperScissors = () => {
 						<div className="px-8 react-chartjs">
 							<Radar
 								data={playerDataChartJS}
-								options={optionsChartJS}
+								options={ChartShotPctOptions}
 								/>
 						</div>
 					</div>
@@ -258,7 +168,7 @@ const RockPaperScissors = () => {
 						<div className="px-8 react-chartjs">
 							<Radar
 								data={cpuDataChartJS}
-								options={optionsChartJS}
+								options={ChartShotPctOptions}
 								/>
 						</div>
 					</div>
