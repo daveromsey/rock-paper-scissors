c85fe273ae75bb634f13dfffb6712c0e2c14b8b5
David Romsey <david.romsey@webdevstudios.com>
2022-02-20 19:46:41 -0500

    Formatting

--- app/src/index.js
+++ app/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
 		<AppProvider>
-   	 <App />
+   	 	<App />
 		</AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
