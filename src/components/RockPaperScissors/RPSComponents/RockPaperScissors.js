8d1ce54269dc54bf81ad783257ed375b8c69b2f1
David Romsey <david.romsey@webdevstudios.com>
2022-02-25 17:54:33 -0500

    Cleamup

--- src/components/RockPaperScissors/RPSComponents/RockPaperScissors.js
+++ src/components/RockPaperScissors/RPSComponents/RockPaperScissors.js
@@ -261,20 +261,15 @@ const RockPaperScissors = () => {
 		/>
 
 		<div className="results py-3 grid grid-cols-1 sm:grid-cols-3 text-xl ">
-			{/* experiment with stacked layout */}
 			<div className="player-shot order-1 mx-auto sm:mx-0">
 				<div className="flex flex-col text-center ">
 					<div className="player">
 						<FaUser className="relative text-[22px] top-[-4px] text-[#25d5f4cc]"/> Your Shot
 					</div>
 					<ShotResult shot={game.playerShot} />
-					{/* { getShotResultComponent( game.playerShot ) } */}
 				</div>
 			</div>
-			{/* simple layout */}
-			{/* <div className="player-shot order-1 mx-auto sm:mx-0">
-				<FaUser className="relative text-[22px] top-[-4px] text-[#25d5f4cc]"/> Player Shot: { getShotOutput( game.playerShot ) }
-			</div> */}
+
 			<div className="game-winner xs:text-center justify-center order-3 xs:order-2 text-2xl font-brand mx-auto sm:mx-0 xs:text-3xl xs:min-h-[56px] mt-2">
 				{ getWinnerComponent( game.winner ) }
 			</div>
@@ -282,16 +277,11 @@ const RockPaperScissors = () => {
 			<div className="cpu-shot xs:text-right order-2 xs:order-3 mx-auto sm:mx-0">
 				<div className="flex flex-col text-center ">
 					<div className="cpu">
-						<FaUser className="relative text-[22px] top-[-4px] text-[#FF7003ee]"/> CPU Shot
+						<FaRobot className="relative text-[22px] top-[-4px] text-[#FF7003ee]"/> CPU Shot
 					</div>
 					<ShotResult shot={game.cpuShot} />
-					{/* { getShotResultComponent( game.cpuShot ) } */}
 				</div>
 			</div>
-			{/* simple layout */}
-			{/* <div className="cpu-shot xs:text-right order-2 xs:order-3 mx-auto sm:mx-0">
-				<FaRobot className="relative text-[26px] top-[-4px] text-[#FF7003ee]"/> CPU Shot: { getShotOutput( game.cpuShot ) }
-			</div> */}
 		</div>
 
 		<RPSPageBreak text={<><FaRegHandRock/> <FaRegHandPaper/> <FaRegHandScissors/></>}/>
