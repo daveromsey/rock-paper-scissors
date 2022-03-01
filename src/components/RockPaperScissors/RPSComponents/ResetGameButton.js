761a8698bed9e70bc638d7a6c2d13f9150db4192
David Romsey <david.romsey@webdevstudios.com>
2022-03-01 00:58:02 -0500

    Formatting

--- src/components/RockPaperScissors/RPSComponents/ResetGameButton.js
+++ src/components/RockPaperScissors/RPSComponents/ResetGameButton.js
@@ -6,13 +6,15 @@ const ResetGameButton = () => {
 	const { resetGame } = useRPSContext();
 
   return (
-		<button onClick={ () => resetGame() } className="ResetGameButton play-again invisible flex-grow mt-8 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-center border rounded-md
+		<button onClick={ () => resetGame() } className="ResetGameButton play-again
+			invisible border rounded-md flex-grow mt-8 mr-2 mb-2 px-5 py-2.5 font-medium text-sm text-center
 			text-retropurple-600 border-retropurple-600
 			hover:text-retropurple-100 hover:bg-retropurple-600
 			dark:text-retropurple-100 dark:border-retropurple-100
 			dark:hover:bg-retropurple-600"
 		>
-			<span className="button-text">Play Again </span><span className="button-icon relative top-[-2px]"><MdDoubleArrow size={20} /></span>
+			<span className="button-text">Play Again </span>
+			<span className="button-icon relative top-[-1px]"><MdDoubleArrow size={20} /></span>
 		</button>
   )
 }
