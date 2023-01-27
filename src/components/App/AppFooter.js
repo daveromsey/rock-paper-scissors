eb5869849d1ad4870ee091798ef947d44a0c9273
David Romsey <david.romsey@webdevstudios.com>
2023-01-27 15:24:21 -0500

    Update copyright year

--- src/components/App/AppFooter.js
+++ src/components/App/AppFooter.js
@@ -31,7 +31,7 @@ const AppFooter = () => {
 			<section className="footer-right my-8 sm:my-2 flex max-w-full justify-center sm:justify-end sm:flex-1 sm:items-end">
 				<p>
 					<span className="site-author">
-						<span className="copyright text-sm">&copy; 2022</span>
+						<span className="copyright text-sm">&copy; 2023</span>
 						<span className="author-name"> &#183; Dave Romsey </span>
 						<a href="https://github.com/daveromsey/rock-paper-scissors">
 							<FaGithub size={24} className="top-[-3px] relative inline-block github"/>
